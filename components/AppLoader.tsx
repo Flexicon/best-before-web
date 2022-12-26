a519c025fcd9356005a9bedc60008c161f6e8e83
Mike Repec <mikerepec@gmail.com>
2022-12-26 19:24:07 +0100

    Remove unused imports

--- components/AppLoader.tsx
+++ components/AppLoader.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
 import { RiFridgeLine } from 'react-icons/ri'
 import { CSSTransition } from 'react-transition-group'
 
