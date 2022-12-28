0f93e5dbb9cdf4182197ffaff09318ac78d7df99
Mike Repec <mikerepec@gmail.com>
2022-12-29 00:30:23 +0100

    Handle overflowing product nmaes

--- components/ProductsListItem.tsx
+++ components/ProductsListItem.tsx
@@ -24,13 +24,13 @@ export const ProductsListItem = ({ product }: Props) => {
       href={`/product/${product.id}`}
       className={`flex items-start justify-between rounded bg-gray-50 py-3 px-4 shadow-md transition-all hover:bg-gray-100 hover:shadow-lg`}
     >
-      <div className="flex items-start gap-2">
+      <div className="flex w-full items-start gap-2">
         <div className="pt-1 text-xl text-gray-500">
           <ProductIcon />
         </div>
 
-        <div className="flex flex-col">
-          <span>{product.name}</span>
+        <div className="flex min-w-0 flex-col">
+          <span className="overflow-hidden text-ellipsis whitespace-nowrap">{product.name}</span>
           <span className="text-xs text-gray-500">{product.expiry_date}</span>
         </div>
       </div>
