3176d416551d72a4b97005fea498b2f13c6fd3ac
Mike Repec <mikerepec@gmail.com>
2022-12-29 00:36:42 +0100

    Prep new product page

---
+++ pages/product/new.tsx
@@ -0,0 +1,23 @@
+const NewProductPage = () => {
+  return (
+    <div className="pt-5">
+      <div className="card w-[500px] max-w-full">
+        <h2 className="mb-6 text-2xl">New Product</h2>
+
+        {/* <ProductForm product={product} disabled={busy} onSubmit={onSubmit} onDelete={onDelete} /> */}
+
+        {/* TODO: remove when appropriate 🤷‍♂️ */}
+        <hr className="my-10" />
+        <div className="font-mono">
+          <p>TODO:</p>
+          <ul className="list-inside list-disc">
+            <li>handling new products via ProductForm</li>
+            <li>submission to api</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NewProductPage
