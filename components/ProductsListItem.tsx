1fd157c88b6ec3ecc8493018076ff4d167faab6e
Mike Repec <mikerepec@gmail.com>
2023-02-03 17:28:05 +0100

    Highlight expired items

--- components/ProductsListItem.tsx
+++ components/ProductsListItem.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { useMemo } from 'react'
 import { IconType } from 'react-icons'
 import { GiMedicinePills, GiPill } from 'react-icons/gi'
-import { IoFastFood } from 'react-icons/io5'
+import { IoFastFood, IoWarningOutline } from 'react-icons/io5'
 
 import type { IconValues, Product } from 'types'
 
@@ -18,11 +18,13 @@ type Props = {
 
 export const ProductsListItem = ({ product }: Props) => {
   const ProductIcon = useMemo(() => icons[product.icon], [product.icon])
+  const isExpired = new Date(product.expiry_date).getTime() < Date.now()
+  const bgClasses = isExpired ? 'bg-red-100 hover:bg-red-200' : 'bg-gray-50 hover:bg-gray-100'
 
   return (
     <Link
       href={`/product/${product.id}`}
-      className={`flex items-start justify-between rounded bg-gray-50 py-3 px-4 shadow-md transition-all hover:bg-gray-100 hover:shadow-lg`}
+      className={`flex items-start  justify-between rounded ${bgClasses} py-3 px-4 shadow-md transition-all hover:shadow-lg`}
     >
       <div className="flex w-full items-start gap-2">
         <div className="pt-1 text-xl text-gray-500">
@@ -31,7 +33,10 @@ export const ProductsListItem = ({ product }: Props) => {
 
         <div className="flex min-w-0 flex-col">
           <span className="overflow-hidden text-ellipsis whitespace-nowrap">{product.name}</span>
-          <span className="text-xs text-gray-500">{product.expiry_date}</span>
+          <span className="flex items-center gap-1 text-xs text-gray-500">
+            {isExpired && <IoWarningOutline />}
+            <span>{product.expiry_date}</span>
+          </span>
         </div>
       </div>
     </Link>
